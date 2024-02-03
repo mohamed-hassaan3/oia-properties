1e1536979c097db789ec92d627fb6153bdad8083
mohamed hassaan <mmhassaan3@gmail.com>
2024-02-04 01:07:08 +0400

    Add Animation

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { GALLERY } from '../data/db'
-
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 const Gallery = () => {
   const [selectedCategory, setSelectedCategory] = useState("Exterior")
-
+  gsap.registerPlugin(ScrollTrigger);
   const handleSelect = (category) => {
     setSelectedCategory(category)
   };
+useEffect(() => {
 
+  const section = document.querySelector('.images');
+  if (section) {
+    // Set up the ScrollTrigger animations for each photo
+    gsap.utils.toArray('.photo').forEach((photo, index) => {
+      const direction = index % 2 === 0 ? 1 : -1; // 1 for right, -1 for left
+      const distance = 100 * direction; // Adjust the distance based on your design
+
+      gsap.to(photo, {
+        scale: 1,
+        x: 0, // Reset x position
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: photo,
+          start: 'top 80%',
+          end: 'bottom 20%',
+          scrub: 0.5,
+        },
+      });
+
+      // Initial position for each photo before the animation starts
+      gsap.set(photo, {
+        x: distance,
+        opacity: 0,
+        scale: 0.5,
+      });
+    });
+  }
+}, [])
 
   return (
     <section id="gallery" className='lg:section w-[80%] m-auto'>
@@ -21,13 +52,13 @@ const Gallery = () => {
             key={category}>{category}</button>
           ))}
         </nav>
-        <figure className='lg:grid space-y-2 grid-cols-2 lg:grid-cols-4 grid-flow-col grid-rows-2 gap-4 *:rounded-3xl'>
+        <figure className='images lg:grid space-y-2 grid-cols-2 lg:grid-cols-4 grid-flow-col grid-rows-2 gap-4 *:rounded-3xl'>
           {GALLERY[selectedCategory].map((item, index) => (
             <img
               alt={item}
               src={item.image}
               key={index}
-              className={`h-[350px] object-cover w-full row-span-${item.full} col-span-${item.cols} `} />
+              className={`photo h-[350px] object-cover w-full row-span-${item.full} col-span-${item.cols} `} />
           ))}
         </figure>
       </article>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Form from './Form'
 import Hero from '../assets/gallery/ex1.webp'
+import {gsap} from 'gsap';
 
 const Home = () => {
+  useEffect(() => {
+    var tl = gsap.timeline()
+    // HERO IMG
+    tl.from('.introImg', {
+      duration: 1, y: "-100%", opacity: .2
+    })
+      .to('.introImg', { duration: 1, y: 0, opacity: 1 })
+    // FORM
+    tl.from('.introForm', {
+      duration: 1, x: "150%", opacity: .2,
+    })
+      .to('.introForm', { duration: 1, x: 0, opacity: 1 })
+    // TITLE
+    tl.from('.introTitle', {
+      duration: 1,  opacity: 0,
+    })
+      .to('.introTitle', { duration: 1, opacity: 1,  })
+  }, []);
   return (
     <section
       id="home">
-      <div className=' bg-no-repeat bg-center bg-cover object-cover lg:w-[80%] h-[600px] w-full m-auto mt-[100px] brightness-50 relative rounded-xl'
-        style={{ backgroundImage: `url(${Hero})`}}></div>
-      <article className='*:text-secondary'>
-        <aside className='absolute top-[50%] md:left-[30%] left-[40%] md:-translate-x-[30%] -translate-x-[40%] -translate-y-[40%] w-[80%] lg:w-[50%] space-y-4 lg:space-y-12 leading-5'>
+      <div className='introImg bg-no-repeat bg-center bg-cover object-cover lg:w-[80%] h-[600px] w-full m-auto mt-[100px] brightness-50 relative rounded-xl'
+        style={{ backgroundImage: `url(${Hero})` }}></div>
+      <article className=' *:text-secondary'>
+        <aside className='introTitle absolute top-[50%] md:left-[30%] left-[40%] md:-translate-x-[30%] -translate-x-[40%] -translate-y-[40%] w-[80%] lg:w-[50%] space-y-4 lg:space-y-12 leading-5'>
           <h1 className='xl:text-6xl text-4xl font-bold tracking-wider'>Saadiyat Lagoons</h1>
           <p className='lg:text-2xl text-xl'>Saadiyat Lagoons is the newest residential addition by Aldar Properties that offers 4, 5 & 6 bedroom villas located at Saadiyat Island, Abu Dhabi.</p>
           <div className='flex flex-wrap items-center gap-3 *:border *:border-secondary *:p-2 *:w-[150px] *:rounded-xl'>
@@ -28,9 +47,9 @@ const Home = () => {
           </div>
         </aside>
       </article>
-        <Form className="-bottom-[450px] absolute right-1/2 translate-x-1/2 my-6 xl:my-0 xl:right-[12%] xl:translate-x-0 xl:bottom-0 xl:w-[25%] md:w-1/2 w-[90%]">
-          <h1 className='text-2xl font-bold text-secondary tracking-wider mb-4'>PROJECT BROCHURE</h1>
-        </Form>
+      <Form className="introForm -bottom-[450px] lg:block hidden absolute right-1/2 translate-x-1/2 my-6 xl:my-0 xl:right-[12%] xl:translate-x-0 xl:bottom-0 xl:w-[25%] md:w-1/2 w-[90%]">
+        <h1 className='text-2xl font-bold text-secondary tracking-wider mb-4'>PROJECT BROCHURE</h1>
+      </Form>
     </section>
   )
 }
